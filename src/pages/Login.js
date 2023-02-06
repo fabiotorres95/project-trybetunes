7a4bf3be03efa1f618bd9105e7c3cf79715f7107
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-06 15:16:54 -0300

    login input is saved in localStorage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+  };
+
+  inputChange = ({ target }) => {
+    const username = target.value;
+    this.setState(() => ({ name: username }));
+  };
+
   render() {
+    const { name } = this.state;
+
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <form>
+          <input
+            onChange={ this.inputChange }
+            type="text"
+            data-testid="login-name-input"
+          />
+          <button
+            onClick={ () => createUser({ name }) }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
