98346f73495c52a96d39e3174d24c6bcdad3b85f
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-06 16:45:18 -0300

    created loading, login input needs length  3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,17 +2,36 @@ import React from 'react';
 import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
-  state = {
-    name: '',
+  constructor() {
+    super();
+
+    this.verify = this.verify.bind(this);
+
+    this.state = {
+      name: '',
+      isNotValid: true,
+    };
+  }
+
+  verify = () => {
+    const { name } = this.state;
+    const charLimit = 3;
+    let result = true;
+    if (name.length >= charLimit) {
+      result = false;
+    }
+
+    this.setState({ isNotValid: result });
   };
 
   inputChange = ({ target }) => {
     const username = target.value;
-    this.setState(() => ({ name: username }));
+    this.setState(() => ({ name: username }), this.verify);
   };
 
   render() {
     const { name } = this.state;
+    const { isNotValid } = this.state;
 
     return (
       <div data-testid="page-login">
@@ -25,6 +44,7 @@ class Login extends React.Component {
           />
           <button
             onClick={ () => createUser({ name }) }
+            disabled={ isNotValid }
             data-testid="login-submit-button"
           >
             Entrar
