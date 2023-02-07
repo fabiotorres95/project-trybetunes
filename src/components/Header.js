61fb6ecb5a43ee19d8663e29b4ee0f17bc14186b
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-07 17:31:44 -0300

    loading and username appears in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from '../Loading';
 
 class Header extends React.Component {
+  state = {
+    isLoading: false,
+    user: '',
+  };
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  loadUser = () => {
+    let user = '';
+    this.setState({ isLoading: true }, async () => {
+      await getUser().then((result) => {
+        user = result.name;
+      });
+      this.setState({ isLoading: false, user });
+    });
+  };
+
   render() {
-    return <header data-testid="header-component" />;
+    const { isLoading } = this.state;
+    const { user } = this.state;
+
+    return isLoading ? <Loading /> : (
+      <header data-testid="header-component">
+        <p data-testid="header-user-name">{ user }</p>
+      </header>
+    );
   }
 }
 
